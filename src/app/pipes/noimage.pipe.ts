20e47e9c642a1810832110acd7498387078cf3f6
TonatiujSanchez <tonsanzjimz@gmail.com>
2020-10-07 02:14:01 -0500

    pipe de noimage corregido

--- src/app/pipes/noimage.pipe.ts
+++ src/app/pipes/noimage.pipe.ts
@@ -8,7 +8,7 @@ export class NoimagePipe implements PipeTransform {
   transform( image: string ): string {
     
     if( image == null || image == undefined ){
-      return './assets/img/no-image.jpg'
+      return 'assets/img/no-image.jpg'
     }    
     
     return 'https://image.tmdb.org/t/p/w600_and_h900_bestv2'+image;
